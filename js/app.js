f52eae09f5cff40a3bcd54a6820af15a44273892
priyankakumbha <pramodkumbhar789@gmail.com>
2015-03-27 21:13:58 +1100

    added stub for streetfighter

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+	
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+	.mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  });
+
+});
